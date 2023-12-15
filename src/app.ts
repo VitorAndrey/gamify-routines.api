65dfa2dc337a6eb8e05bf67b74b9739a6d26769c
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-12-15 12:42:12 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import cors from "@fastify/cors";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.register(cors, {
+  origin: "framercanvas.com",
+  methods: ["POST", "GET", "PUT"],
+});
